35100ab2cf6d29ff4079d707f127adf77cf1633b
Alex Serra Vidal <alex.serra@in2.es>
2020-12-23 14:31:57 +0100

    ADD "CA" translations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {delay} from 'rxjs/operators';
-import {of} from 'rxjs';
+import {of, Subscription} from 'rxjs';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  constructor(private _translate: TranslateService) {
+    _translate.setDefaultLang('ca');
+  }
 
+  firstPhraseTexti18n: string;
+  secondPhraseTexti18n: string;
   firstPhraseTypingFinished: boolean = false;
   secondPhraseTypingFinished: boolean = false;
   startTypingSecondPhrase: boolean = false;
   isVmwButtonEnabled: boolean = false;
+
+  /// SUBSCRIPTIONS
+  sFirstPhraseText: Subscription;
+  sSecondPhraseText: Subscription;
+
+  ngOnInit(): void {
+    this.sFirstPhraseText = this._translate.stream('APP.INTRO_MSG_P1').subscribe((text: string) => {
+      this.firstPhraseTexti18n = text;
+    });
+
+    this.sSecondPhraseText = this._translate.stream('APP.INTRO_MSG_P2').subscribe((text: string) => {
+      this.secondPhraseTexti18n = text;
+    })
+  }
+
+  ngOnDestroy(): void {
+    if (this.sFirstPhraseText) this.sFirstPhraseText.unsubscribe();
+    if (this.sSecondPhraseText) this.sSecondPhraseText.unsubscribe();
+  }
 
   onIntroTextFirstPhraseFinished(): void {
     this.firstPhraseTypingFinished = true;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {TypingEffectModule} from './typing-effect/typing-effect.module';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+// AoT requires an exported function for factories
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -10,6 +18,14 @@ import {TypingEffectModule} from './typing-effect/typing-effect.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient]
+      }
+    }),
     TypingEffectModule
   ],
   providers: [],
